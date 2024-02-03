7ff66f5cec57c5f922c4e6fefa75edf350a880b7
Vikash Kumhar <vikashkumhar13@gmail.com>
2024-02-03 11:52:04 +0530

    footer done

--- src/layout/public/Footer.tsx
+++ src/layout/public/Footer.tsx
@@ -1,176 +1,142 @@
-export const footerArray = [];
+/* eslint-disable @next/next/no-img-element */
+
+import Link from "next/link";
 
 export default function Footer() {
+  const footerAddress1Array = [
+    {
+      title: "Email",
+      desc: "support@store.com",
+    },
+    {
+      title: "Phone",
+      desc: " +323 32434 5334",
+    },
+    {
+      title: "Fax",
+      desc: " ++323 32434 5333",
+    },
+  ];
+  const footerAddress2Array = [
+    {
+      title: "Email",
+      desc: "supportEmail@store.com",
+    },
+    {
+      title: "Phone",
+      desc: " +521 1245 0000",
+    },
+    {
+      title: "Fax",
+      desc: " ++320 8888 3333",
+    },
+  ];
+  const findStoreArray = [
+    "COUPON CODE",
+    "SIGNUP FOR EMAIL",
+    "SITE FEEDBACK",
+    "CAREERS",
+  ];
+  const getHelpArray = [
+    "Order Status",
+    "Shipping and Delivery",
+    "Returns",
+    "Payment Options",
+    "Contact us",
+  ];
+  const productArray = [
+    "Shoes",
+    "Sport Shoes",
+    "Chunky Sneakers",
+    "Formal Boots",
+    "Casual Shoes",
+  ];
   return (
-    <footer className="bg-gray-900 text-white py-8">
-      <div className="container mx-auto flex flex-wrap justify-between">
-        <div className="w-full lg:w-1/4 mb-8 lg:mb-0">
-          <h2 style={{ color: "white" }} className="font-bold mb-4">
-            SOES STORE
-          </h2>
-          <p style={{ color: "white" }}>
-            ADDRESS OFFICE 1
-            <br />
-            460 West 34th Street, 15th floor, New York
-            <br />
-            Email: support@soestore.com
-            <br />
-            Phone: +323.3242.5334
-            <br />
-            Fax: +323.3242.5333
-          </p>
-          <br />
-          <p style={{ color: "white" }}>
-            ADDRESS OFFICE 2
-            <br />
-            PO Box 16122 Collins Victoria 3000 Australia
-            <br />
-            Email: support@soestore.com.au
-            <br />
-            Phone:+322.3242.5336
-            <br />
-            Fax:+323.3242.5333
-          </p>
-        </div>
-        <div className="w-full lg:w-1/4 mb-8 lg:mb-0">
-          <h2 style={{ color: "white" }} className="font-bold mb-4">
-            FIND OUR STORE
-          </h2>
-          <ul>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                COUPON CODE
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                SIGNUP FOR EMAIL
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                SITE FEEDBACK
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                CAREERS
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="w-full lg:w-1/4 mb-8 lg:mb-0">
-          <h2 style={{ color: "white" }} className="font-bold mb-4">
-            GET HELP
-          </h2>
-          <ul>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                Order Status
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                Shipping And Delivery
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                Returns
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                Payment Options
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                Contact Us
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div className="w-full lg:w-1/4 mb-8 lg:mb-0">
-          <h2 style={{ color: "white" }} className="font-bold mb-4">
-            PRODUCTS
-          </h2>
-          <ul>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                Shoes
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                Clothings
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                Accessories
-              </a>
-            </li>
-            <li>
-              <a
-                href="#"
-                style={{ color: "#6B7280" }}
-                className="hover:text-white"
-              >
-                Football Shoes
-              </a>
-            </li>
-          </ul>
+    <div className="grid md:grid-cols-5 grid-cols-1 gap-16 h-[32rem] bg-black text-white">
+      <div className="p-8 flex flex-col col-span-2 gap-12">
+        <img src="/footer_logo.avif" alt="" className="w-fit h-fit" />
+        <div className=" flex admin-gap">
+          <div className="flex flex-col gap-6">
+            <p className="font-medium tracking-wide text-3xl whitespace-nowrap">
+              ADDRESS OFFICE 1
+            </p>
+            <div className="flex flex-col gap-2 text-sm text-gray-300">
+              <p className="whitespace-nowrap">
+                460 West 34th Street, 15th floor, New York
+              </p>
+
+              {footerAddress1Array?.map((data, i) => (
+                <div className="text-white flex gap-2 items-center" key={i}>
+                  <p className="">{data?.title} :</p>
+                  <p className="">{data?.desc}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+          <div className="flex flex-col gap-6">
+            <p className="font-medium tracking-wide text-3xl whitespace-nowrap">
+              ADDRESS OFFICE 2
+            </p>
+            <div className="flex flex-col gap-2 text-sm text-gray-300">
+              <p className=" whitespace-nowrap">
+                PO Box 16122 Collins Victoria 3000 Australia
+              </p>
+
+              {footerAddress2Array?.map((data, i) => (
+                <div className="flex gap-2 items-center" key={i}>
+                  <p className="">{data?.title} :</p>
+                  <p className="">{data?.desc}</p>
+                </div>
+              ))}
+            </div>
+          </div>
         </div>
       </div>
-    </footer>
+      <div className="col-span-1 p-8 flex flex-col admin-gap">
+        <div className="h-20 text-2xl font-semibold whitespace-nowrap">
+          FIND OUR STORE
+        </div>
+        <div className="flex flex-col gap-4">
+          {findStoreArray?.map((data, i) => (
+            <div
+              className="text-gray-300 hover:text-gray-200 font-semibold hover:underline common-transition cursor-pointer"
+              key={i}
+            >
+              <Link href="/">{data}</Link>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="col-span-1 p-8 flex flex-col admin-gap">
+        <div className="h-20 text-2xl font-semibold whitespace-nowrap">
+          PRODUCTS
+        </div>
+        <div className="flex flex-col gap-4">
+          {getHelpArray?.map((data, i) => (
+            <div
+              className="text- font-medium text-gray-300 hover:text-lime-300 hover:underline common-transition cursor-pointer"
+              key={i}
+            >
+              <Link href="/">{data}</Link>
+            </div>
+          ))}
+        </div>
+      </div>
+      <div className="col-span-1 p-8 flex flex-col admin-gap">
+        <div className="h-20 text-2xl font-semibold whitespace-nowrap">
+          GET HELP
+        </div>
+        <div className="flex flex-col gap-4">
+          {productArray?.map((data, i) => (
+            <div
+              className="text- font-medium text-gray-300 hover:text-lime-300 hover:underline common-transition cursor-pointer animate__animated animate__fadeIn animate__faster"
+              key={i}
+            >
+              <Link href="/">{data}</Link>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
   );
 }
